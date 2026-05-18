00bc30074955774cede5dbfc6c6cc9f4416dd320
Pawel Papiez <polokr@gmail.com>
2026-05-18 21:58:10 +0200

    add class and calculate washing price

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
-class Car:
-    # write your code here
-    pass
+from pygments.lexers import clean
 
+
+class Car:
+    def __init__(self, comfort_class : int, clean_mark : int, brand : str):
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self, distance_from_city_center : float, clean_power : int,average_rating : float, count_of_ratings : int):
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def calculate_washing_price(self, car):
+        price = car.comfort_class * (self.clean_power - car.clean_mark) * (self.average_rating / self.distance_from_city_center)
+        return round(price, 1)
+
+    def waswash_single_car
+
+
+
+
+
+
